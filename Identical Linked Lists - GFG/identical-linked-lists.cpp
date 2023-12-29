367f76f2e3372db5f88ad1a41d610979e5e9f267
AditiMishra2003 <115421723+AditiMishra2003@users.noreply.github.com>
2023-12-29 20:02:53 +0530

    Added solution - LeetHub

--- Identical Linked Lists - GFG/identical-linked-lists.cpp
+++ Identical Linked Lists - GFG/identical-linked-lists.cpp
@@ -79,9 +79,11 @@ bool areIdentical(struct Node *head1, struct Node *head2)
     if(head1==NULL && head2==NULL)return true;
     if(size(head1)!=size(head2))return false;
     while(head1!=NULL && head2!=NULL){
-        if(head1->data!=head2->data)return false;
+        if(head1->data==head2->data){
         head1=head1->next;
         head2=head2->next;
+        }
+        else if(head1->data!=head2->data)return false;
     }
     return true;
     
