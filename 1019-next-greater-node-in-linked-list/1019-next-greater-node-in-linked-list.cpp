cc8ca03901932ebcfdbb5f9c036d7aa77a3aea9a
AditiMishra2003 <115421723+AditiMishra2003@users.noreply.github.com>
2024-05-10 00:16:38 +0530

    Time: 785 ms (21.82%), Space: 44.8 MB (45.79%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -13,24 +13,24 @@ public:
     
     vector<int> nextLargerNodes(ListNode* head) {
         vector<int> v;
+        
         ListNode* temp=head;
         while(temp!=NULL){
             v.push_back(temp->val);
             temp=temp->next;
         }
         int n=v.size();
-        for(int i=0;i<=n-2;i++){
-            int x=v[i];
-            for(int j=i+1;j<=n-1;j++){
-                if(v[j]>x){
-                    v[i]=v[j];
+        vector<int> ans(n,0);
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(v[j]>v[i]){
+                    ans[i]=v[j];
                     break;
                 }
-              
             }
-            if(v[i]==x)v[i]=0;
+            
         }
-        v[n-1]=0;
-        return v;
+        ans[n-1]=0;
+        return ans;
     }
 };
